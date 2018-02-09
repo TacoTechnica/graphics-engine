cda83188e17a5739db595ad148350616aa7e2dfc
Adris <adrisj7@gmail.com>
2018-02-09 15:17:46 -0500

    Add line algorithm, it works.

--- src/picmaker.cpp
+++ src/picmaker.cpp
@@ -69,9 +69,98 @@ void Image::writeToPPM(Image *img, const char* name) {
     }
 }
 
+void plot(Image *img, int x, int y, struct Pixel *color) {
+    memcpy(img->getPixel(x,y), color, sizeof(struct Pixel));
+}
+
+// TODO: Test Octant 2
+void draw_line(Image *img, int x0, int y0, int x1, int y1, struct Pixel *color) {
+    // swapping
+    int swap;
+    if (x0 > x1) {
+        swap = x0;
+        x0 = x1;
+        x1 = swap;
+        swap = y0;
+        y0 = y1;
+        y1 = swap;
+    }
+
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int a = dy; // TODO: You can remove a and b...
+    int b = -dx;
+    int d;
+
+    if (dy >= 0) { // Quadrant 1 and 3
+        // Octant 1 and 5
+        if (dy <= dx) {
+            d = 2*a + b;
+            while(x0 <= x1) {
+                plot(img, x0, y0, color);
+                if (d > 0) {
+                    y0++;
+                    d += 2*b;
+                }
+                x0++;
+                d += 2*a;
+            }
+        // Octant 2 and 6
+        } else {
+            d = a + 2*b;
+            while(y0 <= y1) {
+                plot(img, x0, y0, color);
+                if (d < 0) {
+                    x0++;
+                    d += 2*a;
+                }
+                y0++;
+                d += 2*b;
+            }
+            
+        }
+        return;
+    } else { // Quadrant 2 and 4 
+        // Octant 4 and 8
+        if (-dy <= dx) {
+            d = -2*a + b;
+            while(x0 <= x1) {
+                plot(img, x0, y0, color);
+                if (d > 0) {
+                    y0--;
+                    d += 2*b;
+                }
+                x0++;
+                d -= 2*a;
+            }
+        // Octant 2 and 6
+        } else {
+            d = -a + 2*b;
+            while(y0 >= y1) {
+                plot(img, x0, y0, color);
+                if (d < 0) {
+                    x0++;
+                    d -= 2*a;
+                }
+                y0--;
+                d += 2*b;
+            }
+        }
+        return;
+    }
+}
+
+
 int main() {
     Image img(320,320);
 
+    struct Pixel p = {255, 255, 255};
+
+    double theta;
+    double r = 100.0;
+    for(theta = 0; theta <= 2.0 * 3.1415; theta += 3.1415 / 10) {
+        draw_line(&img, 150, 150, 150 + (int)(r * cos(theta)), 150 + (int)(r * sin(theta)), &p);
+    }
 
     Image::writeToPPM(&img, "sp00k.ppm");
 
